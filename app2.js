f6221de579802d1cc21842dc6780a843f0ecbb16
KaLaUnlimited <KLasode@gmail.com>
2018-01-25 20:37:17 -0500

    table changes are made in firebase from temp2.html next step: append to dom

--- app2.js
+++ app2.js
@@ -1,5 +1,5 @@
 //   // Initialize Firebase
-alert();
+
 
 
 var flightData_ref= firebase.database().ref('Site/FlightData/')
@@ -31,19 +31,20 @@ flightData_ref
   .once('value')
   .then(function(records) {
   var recObj = records.val();
-  console.log("Objects: " +recObj);
+  console.log("Key: " + records.key)
+  console.log(recObj);
   $("#search-report-table > tbody").empty();
 
 for(var child in recObj){
  //flightData_ref.on("child_added", function(records){
- 	console.log("this is the key: " +recObj[child])
-  $("#search-report-table > tbody").append("<tr data-key="+"><td>" + recObj[child].dateLogged + "</td><td>"+ recObj[child].loggedBy
+ 	console.log("this is the child key: " +child);
+  $("#search-report-table > tbody").append("<tr data-key="+ child+ "><td>" + recObj[child].dateLogged + "</td><td>"+ recObj[child].loggedBy
   + "</td><td>" +recObj[child].timeLogged+ "</td><td>" +recObj[child].system+ "</td><td>"  + recObj[child].systemStatus + "</td><td>" 
   + recObj[child].flightAltitude + "</td><td>" + recObj[child].reasonMoored + "</td><td>" 
   + recObj[child].Launches + "</td><td>"+ recObj[child].recoveries+ "</td><td>" +recObj[child].tetherTension + "</td><td>" +recObj[child].groundWinds 
   + "</td><td>"+recObj[child].windsAloft + "</td><td>"+recObj[child].groundTemp + "</td><td>"+ recObj[child].barometricPressure + "</td><td>"+recObj[child].pitch 
-  + "</td><td>"+recObj[child].heliumPressure + "</td><td>"+recObj[child].ballonetPressure + "</td><td>"+recObj[child].notes + "</td><td><button data-key="
-  + recObj[child].key + ">Update Record</button></td></tr>" );
+  + "</td><td>"+recObj[child].heliumPressure + "</td><td>"+recObj[child].ballonetPressure + "</td><td>"+recObj[child].notes + "</td><td><button id='upDateRec' data-toggle='modal' data-target='#exampleModal' data-key="
+  + child + ">Update Record</button></td></tr>" );
 
 
 }
@@ -75,32 +76,69 @@ for(var child in recObj){
 
 
 
-
+var key;
 /////////////////////////////////////////////////////////////////////////update/////////////////////////////////////////
 
+$(document).on('click', "#upDateRec", function(){
+	console.log("test");
+	key = $(this).parents('tr').data('key');
+	console.log("upate button" + key);
 
-// function updateItem(key) {
-//    dateLogged =$("#Date-Input").val().trim();
-//    timeLogged =$("#Time-Logged-Input").val().trim();
-//    loggedBy=$("#Logged-By-Input").val().trim();
-//    systemStatus=$("#System-Status-Input").val().trim();
-//    flightAltitude=$("#Flight-Altitude-Input").val().trim();
-//    reasonMoored=$("#Reason-Moored-Input").val().trim();
-//    Launches =$("#Launches-Input").val().trim();
-//    recoveries=$("#Recoveries-Input").val().trim();
-//    tetherTension=$("#Tether-Tension-Input").val().trim();
-//    groundWinds=$("#Ground-Winds-Input").val().trim();
-//    windsAloft =$("#Winds-Aloft-Input").val().trim();
-//    groundTemp=$("#Ground-Temp-Input").val().trim();
-//    barometricPressure=$("#Barometric-Pressure-Input").val().trim();
-//    pitch =$("#Pitch-Input").val().trim();
-//    heliumPressure=$("#Helium-Pressure-Input").val().trim();
-//    ballonetPressure=$("#Ballonet-Pressure-Input").val().trim();
-//    notes= $("#Notes").val().trim();
-// 	flightData_ref.child(key).set({
-// 		title: title,
-// 		date: date
-// 	});
+});
+
+$("#saveModal").on("click", function(){
+
+   dateLogged =$("#Date-Input").val().trim();
+   timeLogged =$("#Time-Logged-Input").val().trim();
+   loggedBy=$("#Logged-By-Input").val().trim();
+   systemStatus=$("#System-Status-Input").val().trim();
+   flightAltitude=$("#Flight-Altitude-Input").val().trim();
+   reasonMoored=$("#Reason-Moored-Input").val().trim();
+   Launches =$("#Launches-Input").val().trim();
+   recoveries=$("#Recoveries-Input").val().trim();
+   tetherTension=$("#Tether-Tension-Input").val().trim();
+   groundWinds=$("#Ground-Winds-Input").val().trim();
+   windsAloft =$("#Winds-Aloft-Input").val().trim();
+   groundTemp=$("#Ground-Temp-Input").val().trim();
+   barometricPressure=$("#Barometric-Pressure-Input").val().trim();
+   pitch =$("#Pitch-Input").val().trim();
+   heliumPressure=$("#Helium-Pressure-Input").val().trim();
+   ballonetPressure=$("#Ballonet-Pressure-Input").val().trim();
+   notes= $("#Notes").val().trim();
+	
+
+
+	console.log(dateLogged)
+
+	console.log(key);
+
+
+	   flightData_ref.child(key).set({
+	   dateLogged: dateLogged,
+	   timeLogged: timeLogged,
+	   loggedBy:loggedBy,
+	 //  system:system,
+	   systemStatus:systemStatus,
+	   flightAltitude:flightAltitude,
+	   reasonMoored:reasonMoored,
+	   Launches:Launches,
+	   recoveries:recoveries,
+	   tetherTension:tetherTension,
+	   groundWinds:groundWinds,
+	   windsAloft:windsAloft,
+	   groundTemp:groundTemp,
+	   barometricPressure:barometricPressure,
+	   pitch:pitch,
+	   heliumPressure:heliumPressure,
+	   ballonetPressure:ballonetPressure,
+	   notes:notes
+	});
+
+	});
+
+
+
+
 
 // 	$(`tr[data-key="${key}"]`).after(`
 // 		<tr data-key="${key}">
@@ -145,10 +183,10 @@ for(var child in recObj){
 
 
 
-// function test(some_string) {
-// 	console.log(some_string);
-// }
-// test('some value')
+function test(some_string) {
+	console.log(some_string);
+}
+test('some value')
 
 
 
