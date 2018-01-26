645f950766fd1fd8811858cf47fee0ddeb7c551b
unknown <blkorneg@hotmail.com>
2018-01-25 19:35:44 -0500

    More updates to operations

--- assets/firebase/heliumDB.js
+++ assets/firebase/heliumDB.js
@@ -11,12 +11,9 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// Create a variable to reference the database
-//var database = firebase.database();
-
 // Initial Values
 //Save Helium Data info
-// 2. Button for adding Employees
+
 function saveHeliumData() {
 
  event.preventDefault();
@@ -43,34 +40,91 @@ function saveHeliumData() {
   }
 
 firebase.database().ref('Site/HeliumData/').push(HeliumData);
-.log("Update Success");
+console.log("Update Success");
 }
 
 
-firebase.database().ref('Site/HeliumData/').on("child_added", function(childSnapshot, prevChildKey) {
+firebase.database().ref('Site/HeliumData/').limitToLast(1).on('child_added', function(snapshot) {
 
-  console.log(childSnapshot.val());
+  console.log(snapshot.val());
 
   // Store everything into a variable.
 
-  var cont1 = childSnapshot.val().HeliumContainer1;
-  var cont2 = childSnapshot.val().HeliumContainer2;
-  var cont3 = childSnapshot.val().HeliumContainer3;
-  var cont4 = childSnapshot.val().HeliumContainer4;
-  var inEx = childSnapshot.val().HeliumExpended;
-  var loggedBy = childSnapshot.val().LoggedBy;
-  var note = childSnapshot.val().Notes;
-  var inTLog = childSnapshot.val().TimedLogged;
+  var cont1 = snapshot.val().HeliumContainer1;
+  var cont2 = snapshot.val().HeliumContainer2;
+  var cont3 = snapshot.val().HeliumContainer3;
+  var cont4 = snapshot.val().HeliumContainer4;
+  var inEx = snapshot.val().HeliumExpended;
+  var loggedBy = snapshot.val().LoggedBy;
+  var note = snapshot.val().Notes;
+  var inTLog = snapshot.val().TimedLogged;
 
 
-  // Employee Info
-  console.log("Testing reading " + cont1);
-  console.log("Testing reading " + cont2);
-  console.log("Testing reading " + cont3);
-  console.log("Testing reading " + cont4);
-
-
-  // Add each train's data into the table
-  $("#helium-status-table > tbody").append("<tr><td>" + cont1 + "</td><td>" + cont2 + "</td><td>" +
+  // Add each helium data into the table
+  $("#helium-status-table > tbody").append("<tr><td>" + inTLog + "</td><td>" + cont1 + "</td><td>" + cont2 + "</td><td>" +
   cont3 + "</td><td>" + cont4 + "</td><td>" + inEx + "</td></tr>");
+      //SwapDivsWithClick('formId','heliumTableID');
 });
+
+
+
+/* Charting Data**/
+var groundWinds = [];
+var windsAloft = [];
+var flightAltitude = [];
+var tetherTension = [];
+
+firebase.database().ref('Site/FlightData/').orderByChild("dateLogged")//dateLogged)
+  .limitToLast(24)
+  .once('value')
+  .then(function(records) {
+  var recObj = records.val();
+  $("#search-report-table > tbody").empty();
+  for(var child in recObj){
+    console.log("groundWinds " + recObj[child].groundWinds);
+    groundWinds.push(recObj[child].groundWinds);
+    windsAloft.push(recObj[child].windsAloft);
+    flightAltitude.push(recObj[child].flightAltitude);
+    tetherTension.push(recObj[child].tetherTension);
+  }
+});
+
+
+var ctx = document.getElementById("line-chart");
+    console.log ("Bradshaw :" + ctx);
+
+    var lnChart = new Chart(ctx, {
+        type: 'bar',
+		        type: 'line',
+		  data: {
+		    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+		    datasets: [{
+		        data: groundWinds,
+		        label: "Ground Wind Speed",
+		        borderColor: "#3e95cd",
+		        fill: false
+		      }, {
+		        data: flightAltitude,
+		        label: "Airborne Wind Speed",
+		        borderColor: "#8e5ea2",
+		        fill: false
+		      }, {
+		        data: windsAloft,
+		        label: "API ind Speed",
+		        borderColor: "#3cba9f",
+		        fill: false
+		      }, {
+		        data: tetherTension,
+		        label: "Tether Tension",
+		        borderColor: "#e8c3b9",
+		        fill: false
+		      }
+		    ]
+		  },
+        options: {
+          title: {
+		      display: true,
+		      text: 'Top Gun System Metrics'
+		    }
+		  }
+    });
